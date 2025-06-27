f734e304b813d85fa11f63218de6bc9a9d625b98
ZahYah <adebayoisaiah256@gmail.com>
2025-06-27 15:00:16 +0100

    Woking side-bar using anchor link

--- script/side-bar.js
+++
@@ -1,8 +0,0 @@
-const user = document.getElementById("user-data");
-user.addEventListener("click", function (){
-window.open('../user.html')
-})
-const dashboard = document.getElementById("dashboard");
-dashboard.addEventListener("click", function(){
-  window.open('../dashboard.html')
-})
